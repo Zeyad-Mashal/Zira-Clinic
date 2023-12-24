34c714d9a3da355618efd8b92f7f1d912deac08f
Mashal <zyadomar112@gmail.com>
2023-12-24 21:52:14 +0200

    uppdate

---
+++ translation.js
@@ -0,0 +1,25 @@
+// Translations 
+const translations = {
+    en: {
+        home: "Home",
+        about: "About Us",
+        contact: "Contact",
+        gallary: "Gallary",
+        rates: "Our Rates",
+        reservationBtn: "Book A Reservation",
+        en: "English",
+        ar: "Arabic"
+    },
+    ar: {
+        home: "الرئيسية",
+        about: "من نحن ؟",
+        contact: "اتصل بنا ",
+        gallary : "معرض الصور",
+        rates: "اراء العملاء",
+        reservationBtn: "احجز معانا",
+        en: "الانجليزية",
+        ar: "العربية"
+    }
+}
+
+export default translations;
